2c9f61205468fdd3d2c32a6e0fdb32130a1ff071
mbalpande <mbalpande@IN-PG02LCDL>
2021-11-02 17:42:35 +0530

    all

---
+++ springjdbc/src/main/java/dxc/b2/jdbc/DxcjdbcApplication.java
@@ -0,0 +1,15 @@
+package dxc.b2.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DxcjdbcApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Dxcb2jdbcApplication.class, args);
+	}
+
+
+}
